382b8b48819c6df1523584288484f7beb0891053
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-10-28 18:47:08 +0000

    basic auth ms setup init

---
+++ auth/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
