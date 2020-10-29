e56bd3bc20c60611636d775c09b63932b765ae0d
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-10-29 11:15:56 +0000

    nginx ingress k8s setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,5 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentUser', (req, res) => {
+  res.send('Hi there');
+});
+
+
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!!!`));
