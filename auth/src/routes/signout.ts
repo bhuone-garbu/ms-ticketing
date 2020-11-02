48c9893f8d6116c3b563188edb8179ea4b00c948
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-02 16:33:28 +0000

    handling signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import cookieSession from 'cookie-session';
 
 const router = Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there');
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
