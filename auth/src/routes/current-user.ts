38c5e11cb8b658874caa15901f972c39cf3003c4
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-02 16:47:44 +0000

    current user handler and augmenting the TypeScript type definitions for Express Request interface

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 
+import { currentUserHandler } from '../middlewares/current-user-handler'
+
 const router = Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try{
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null})
-  }
+router.get('/api/users/currentuser', currentUserHandler, (req, res) => {
+  res.send({ currentUser: req.currentUser || null})
 });
 
 export { router as currentUserRouter };
