aaba775ab15edd6f15f4292f9a6f45757444e352
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-05 00:24:05 +0000

    end of testing for current user and refactor to use cookie authentication

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>
+    }
+  }
+}
 
 let mongo: MongoMemoryServer;
 beforeAll(async () => {
@@ -24,7 +34,23 @@ beforeEach(async () => {
   }
 });
 
-afterAll(async() => {
+afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+}
