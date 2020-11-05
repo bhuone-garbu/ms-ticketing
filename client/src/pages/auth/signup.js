3c93f1485e5b565a03308751bc3824b947a18022
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-05 11:59:20 +0000

    adding a simple error display on auth signup

--- client/src/pages/auth/signup.js
+++ client/src/pages/auth/signup.js
@@ -4,14 +4,18 @@ import axios from 'axios';
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    });
-    console.log(response.data);
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
+    } catch (err) {
+      setErrors(err.response.data.errors)
+    }
   }
 
   return (
@@ -32,6 +36,13 @@ const Signup = () => {
           onChange={e => setPassword(e.target.value)}
           className="form-control" />
       </div>
+      {errors.length && (
+        <div className="alert alert-danger">
+          <h4>Opps....</h4>
+          <ul className="my-0">
+            {errors.map(err => <li key={err.message}>{err.message}</li>)}
+          </ul>
+        </div>)}
       <button className="btn btn-primary">Sign Up</button>
     </form>);
 };
