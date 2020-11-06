6d48094d6b13d9dba914a658223f0820f3feeb8b
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-06 18:40:22 +0000

    making the test pass

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -5,34 +5,73 @@ it('has a route handler listening to /api/tickets for post requests', async () =
   const response = await request(app)
     .post('/api/tickets')
     .send({});
-  
+
   expect(response.status).not.toEqual(404);
 });
 
 it('can only be accessed if the user is signed in', async () => {
   await request(app)
-  .post('/api/tickets')
-  .send({})
-  .expect(401);
+    .post('/api/tickets')
+    .send({})
+    .expect(401);
 });
 
 it('returns status other than 401 if the user is signed', async () => {
   const response = await request(app)
-  .post('/api/tickets')
-  .set('Cookie', global.signin())
-  .send({})
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
 
   expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'some title',
+      price: -10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'some title',
+    })
+    .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
+  // add in a check to make sure a ticket was saved
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'some title',
+      price: 20,
+    })
+    .expect(201);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,20 @@
 import { Router, Request, Response } from 'express';
-import { requireAuth } from '@bhuone/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@bhuone/common';
 
 const router = Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+    body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0'),
+], validateRequest, (req: Request, res: Response) => {
+  
+  res.sendStatus(201);
 });
 
 export { router as createTicketRouter };
