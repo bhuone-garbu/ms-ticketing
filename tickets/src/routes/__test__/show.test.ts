0e024ae303c3ec5f2f2108fb1db7c3bb2226b442
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-07 11:56:11 +0000

    updating the test

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 404 if the ticket is not found', async () => {
+  await request(app)
+    .get('/api/tickets/testing123')
+    .send()
+    .expect(404);
+});
+
+it('returns the ticket if the ticket is found', async () => {
+
+  const title = 'some title';
+  const price = 40;
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price
+    })
+    .expect(201);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'some title',
+      price: 40
+    })
+    .expect(200);
+
+  expect(ticketResponse.body.title).toEqual(title);
+  expect(ticketResponse.body.price).toEqual(price);
+});
---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import { NotFoundError } from '@bhuone/common';
+
+import { Ticket } from '../models/ticket';
+
+const router = Router();
+
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) throw new NotFoundError();
+
+  res.send(ticket);
+});
+
+export { router as showTicketRouter };
