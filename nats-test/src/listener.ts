d447616f717bfa55a328e5bd379d82427d53ba9e
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-09 21:23:48 +0000

    custom event interfact refactoring

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,6 +1,7 @@
-import nats, { Message, Stan } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
-import { PassThrough } from 'stream';
+
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -17,66 +18,9 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('test-service');
-  const subscription = stan.subscribe('ticket:created', 'some-queue-group', options);
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-    
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  protected ackWait = 5 * 1000;
-  abstract onMessage(data: any, msg: Message): void;
-
-  constructor(private client: Stan) {}
-
-  subscriptionOptions() {
-    return this.client.subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on('message', (msg: Message) => {
-      console.log(
-        `Message received: ${this.subject} / ${this.queueGroupName}`
-      );
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === 'string'
-      ? JSON.parse(data)
-      : JSON.parse(data.toString('utf-8'));
-  }
-
-}
