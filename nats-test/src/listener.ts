54d06210e67b59ac4a38f443e51db8791a8b94f2
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-09 19:42:56 +0000

    template abstract listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,8 +1,10 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { PassThrough } from 'stream';
 
 console.clear();
 
+// stan is reversed nats, which means a client 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
@@ -35,3 +37,46 @@ stan.on('connect', () => {
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  protected ackWait = 5 * 1000;
+  abstract onMessage(data: any, msg: Message): void;
+
+  constructor(private client: Stan) {}
+
+  subscriptionOptions() {
+    return this.client.subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf-8'));
+  }
+
+}
