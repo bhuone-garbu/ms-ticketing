0224ad6b77bd7a8702a9188c22ef5946f606efd5
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-10 21:32:59 +0000

    updating the ticket-deploy to use environment variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,9 +12,22 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI not defined');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID not defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID not defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL not defined');
+  }
 
   try {
-    await natsWrapper.connect('ticketing', 'abcdef', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL);
+
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
@@ -22,7 +35,7 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
-  
+
     await mongoose.connect(process.env.MONGO_URI, {
       useCreateIndex: true,
       useNewUrlParser: true,
