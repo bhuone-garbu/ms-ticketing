6a3e317b522bf0f5b3db9e19eb18eaa608f8c9ff
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-11 14:11:03 +0000

    skaffolding the new orders micro service

---
+++ orders/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUserHandler } from '@bhuone/common';
+
+// import { createTicketRouter } from './routes/new';
+// import { showTicketRouter } from './routes/show';
+// import { allTicketRouter } from './routes/all';
+// import { saveTicketRouter } from './routes/save';
+
+const app = express();
+app.set('trust proxy', true); //express is aware that it's behind a proxy
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}));
+
+app.use(currentUserHandler);
+
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(allTicketRouter);
+// app.use(saveTicketRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError;
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const PORT = 4000;
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI not defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID not defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID not defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL not defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL);
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to mongodb');
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+}
+
+start();
