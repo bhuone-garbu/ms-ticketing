5404d6ff05b3d9bd0047faedfd83221038335cf3
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-11 19:56:08 +0000

    TDD for new order handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -11,6 +11,9 @@ import { Order } from '../models/order';
 
 const router = Router();
 
+// in seconds
+const EXPIRATION_WINDOW = 15 * 60;
+
 router.post('/api/orders', requireAuth,
   [
     body('ticketId')
@@ -23,14 +26,30 @@ router.post('/api/orders', requireAuth,
   async (req: Request, res: Response) => {
 
     const { ticketId } = req.body;
-    
+
     const ticket = await Ticket.findById(ticketId);
     if (!ticket) throw new NotFoundError();
 
     const isReserved = await ticket.isReserved();
     if (isReserved) throw new BadRequestError('Ticket is already reseved');
 
-    res.status(201).send({});
+    // calculate an expiration date of this order, 15 mins from now
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW);
+
+    // build order and save to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    });
+
+    await order.save();
+
+    // TODO: publish event
+
+    res.status(201).send(order);
   });
 
 export { router as createOrderRouter };
