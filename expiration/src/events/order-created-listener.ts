98d24d12444dd48816aedc20043fa0e0976e5138
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-13 23:44:14 +0000

    adding queue group name in a separate file

---
+++ expiration/src/events/order-created-listener.ts
@@ -0,0 +1,13 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@bhuone/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+
+    msg.ack();
+  }
+}
