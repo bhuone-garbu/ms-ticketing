e9090a0f69c1ad74374080f8575c74e5fe0489d0
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-14 14:59:18 +0000

    stub Order model

---
+++ payments/src/models/order.ts
@@ -0,0 +1,61 @@
+import mongoose, { version } from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { OrderStatus } from '@bhuone/common';
+
+interface OrderAttrs {
+  id: string;
+  status: OrderStatus;
+  version: number;
+  price: number;
+  userId: string;
+}
+
+interface OrderDoc extends mongoose.Document {
+  status: OrderStatus;
+  version: number;
+  price: number;
+  userId: string;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema({
+  status: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    }
+  }
+});
+
+orderSchema.set('versionKey', version);
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) {
+  return new Order({
+    _id: attrs.id,
+    status: attrs.status,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
