ada0e57ed442b4d2d1671c5445d281b8a3c73d3e
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-14 19:20:04 +0000

    upgrading the common package with new payment created event

--- common/src/index.ts
+++ common/src/index.ts
@@ -13,11 +13,13 @@ export * from './middlewares/validate-request';
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
+export * from './events/types/order-status';
+
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/order-created-event';
 export * from './events/order-cancelled-event';
 export * from './events/order-cancelled-event';
-export * from './events/expiration-complete';
+export * from './events/payment-created-event';
+export * from './events/expiration-complete-event';
 
-export * from './events/types/order-status';
