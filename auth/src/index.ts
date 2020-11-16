f53f2a5e56ec0419f3c34808c09dd3cbc2af07c7
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-11-16 14:56:19 +0000

    test to trigger GH action

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,12 +6,12 @@ const PORT = 4000;
 
 const start = async () => {
 
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI not defined');
-  }
-
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI not defined');
   }
 
   try {
